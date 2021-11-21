4cfbbe35bbf015a5cc95d72d4a4e806f8ed99ffe
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-11-21 19:53:39 +0200

    сделал задание

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
 import './css/styles.css';
 import './css/index.css';
+import fetchCountries from './js/api.js';
+import { refs } from './js/refs.js';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.2.min.css';
-import fetchCountries from './js/api.js';
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
-const refs = {
-  input: document.querySelector('#search-box'),
-  ul: document.querySelector('.country-list'),
-};
 
 refs.input.addEventListener('input', debounce(onSearch, 300));
 
 function onSearch(e) {
-  const inputValue = e.target.value;
+  const inputValue = e.target.value.trim();
   console.log(inputValue);
-  fetchCountries(inputValue).then(data => renderListMarkup(data));
 
-  if (inputValue.length <= 0) {
-    return (refs.ul.innerHTML = '');
+  fetchCountries(inputValue).then(data => renderMarkup(data));
+
+  if (inputValue.length === 0) {
+    return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = '');
+  }
+}
+function renderMarkup(countries) {
+  if (countries.length > 10) {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    return Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
+  }
+  if (countries.length >= 2 && countries.length <= 10) {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    return renderListMarkup(countries);
+  }
+  if (countries.length <= 1) {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    return renderInfoMarkup(countries);
   }
 }
 
@@ -32,10 +47,8 @@ function renderListMarkup(countries) {
 `;
     })
     .join('');
-  if (countries.length > 10) {
-    return Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
-  }
-  refs.ul.insertAdjacentHTML('beforeend', markup);
+
+  refs.countryList.insertAdjacentHTML('beforeend', markup);
 }
 function renderInfoMarkup(countries) {
   const markup = countries
@@ -43,16 +56,18 @@ function renderInfoMarkup(countries) {
       console.log(flags.svg);
       return `<div class="country">
 <img class="country-flag" src="${flags.svg}" alt="Flag of ${name.official}>
-<h3 class="country-name">${name.official}</h3>
+<h3 class="country-name--info">${name.official}</h3>
 <ul class="country-list">
 <li class="country-item"><span class="country-item--span">Capital</span>:${capital}</li>
 <li class="country-item"><span class="country-item--span">Population</span>:${population}</li>
-<li class="country-item"><span class="country-item--span">Languages</span>:${languages}</li>
+<li class="country-item"><span class="country-item--span">Languages</span>:${Object.values(
+        languages,
+      )}</li>
 </ul>
 </div>
 `;
     })
     .join('');
 
-  refs.ul.insertAdjacentHTML('beforeend', markup);
+  refs.countryInfo.insertAdjacentHTML('beforeend', markup);
 }
--- src/js/api.js
+++ src/js/api.js
@@ -1,8 +1,14 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+import { refs } from './refs';
 export default function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
   ).then(response => {
     if (!response.ok) {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
       throw new Error(response.status);
     }
     return response.json();
